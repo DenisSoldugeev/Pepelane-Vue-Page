308432c5e61867db657c807e0d5b5a7347efa778
Denis <soldugeev.d@cronix.ms>
2020-08-30 05:50:29 +0300

    init_and_add_header

---
+++ components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="header">
+  <div class="header__container container">
+   <NuxtLink to="/">Logo</NuxtLink>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Header/Msg.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "msg"
+    }
+</script>
+
+<style scoped>
+
+</style>
